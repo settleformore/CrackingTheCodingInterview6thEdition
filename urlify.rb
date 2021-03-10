98a843a00237458b60239b00df6fcd3a33720669
settleformore <ssettle93@gmail.com>
2021-03-10 15:09:43 +0000

    initial commit

---
+++ urlify.rb
@@ -0,0 +1,5 @@
+# Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the addtional characters, and that you are given the "true" length of the string.
+
+# Example:
+# Input: "Mr John Smith      ", 13
+# Output: "Mr%20John%20Smith" 
